5bc42afae74c2528bc63327b42b7f5048813ac27
JavierGo <jg0359131@gmail.com>
2017-01-29 12:37:54 -0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,13 +1,1 @@
-jQuery(document).ready(function() {
-    var navOffset = jQuery("nav").offset().top;
-    
-    jQuery(window).scroll(function() {
-        var scrollPos = jQuery(window).scrollTop();
-        
-        if (scrollPos >= navOffset) {
-            jQuery("nav").addclass("fixed");
-        } else {
-            jQuery("nav").removeClass("fixed");
-        }
-    });
-});
+
