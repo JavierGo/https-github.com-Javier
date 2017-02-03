5e080dd0be938671f043947d1c14aa9699878c18
JavierGo <jg0359131@gmail.com>
2017-02-02 22:56:04 -0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,26 @@
-
+function renderTime() {
+  var currentTime = new Data();
+  var diem = "AM";
+  var h = currentTime.getHours();
+  var m = currentTime.getMinutes();
+  var s = currentTime.getSeconds();
+  
+  if (h == 0) {
+    h= 12;
+  } else if (h > 12) {
+     h = h - 12;
+    diem = "PM";
+  }
+  if (h < 10) { 
+      h = "0" + h;
+  }
+  if (m < 10) {
+      m = "0" + m;
+  }
+  if (s < 10) {
+    s + "0" + s;
+  }
+                       
+  setTimeout('renderTime()',1000);
+}
+renderTime();
